c5d7bb75474a253527a4ae7f6ef0bd66860f6448
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:20:58 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan11_Formatting/src/pbo3_10117098_latihan11_formatting/PBO3_10117098_Latihan11_Formatting.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan11_formatting;
+
+/**
+ * 
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan11_Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus =-i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+        
+    }
+    
+}
